4bc3c1797bc4c72bfc94c053d6f8dc38f3b83f8e
David Murashka <murashka.david@student.ehu.lt>
2024-11-28 14:47:39 +0200

    test

--- scriptchat.js
+++ scriptchat.js
@@ -5,6 +5,8 @@ const socket = io('https://vilija.onrender.com', {
     },
 });
 
+
+
 let offset = 0;
 let loadingHistory = false;
 
@@ -26,7 +28,6 @@ socket.on('connect_error', (err) => {
 
 let secretKey = ''; //сам ключ
 
-
 let SECRET_KEY = '';//для расшыфроўкі ключа(nое што ўводзіць крыстальнік)
 
 // Функцыя для запыту і дэшыфроўкі ключа
@@ -41,8 +42,6 @@ async function fetchAndDecryptKey(SECRET_KEY) {
             const bytes = CryptoJS.AES.decrypt(data.key, SECRET_KEY);
             secretKey = bytes.toString(CryptoJS.enc.Utf8);
 
-            
-
             if (!secretKey) {
                 console.error('Дэшыфроўка вярнула пусты ключ. Праверце SECRET_KEY.');
             }
@@ -53,6 +52,7 @@ async function fetchAndDecryptKey(SECRET_KEY) {
         console.error('Памылка падчас запыту або дэшыфроўкі:', error);
     }
 }
+
 
 
 
@@ -74,6 +74,8 @@ async function initializeChat() {
         console.error('Немагчыма дэшыфраваць ключ.');
     }
 }
+
+
 
 
 
@@ -113,6 +115,17 @@ async function setSecretKey() {
 }
 
 
+function encryptMessage(message, secretKey) {
+    // Шыфруем паведамленне з дапамогай AES і пераўтворым у радок
+    return CryptoJS.AES.encrypt(message, secretKey).toString();
+}
+
+function decryptMessage(encryptedMessage, secretKey) {
+    const bytes = CryptoJS.AES.decrypt(encryptedMessage, secretKey);
+    return bytes.toString(CryptoJS.enc.Utf8);
+}
+
+
 
 
 
@@ -127,21 +140,6 @@ function formatTime(dateString) {
     const date = new Date(dateString); // Пераўтворыце timestamp у аб'ект Date
     return new Intl.DateTimeFormat('en-GB', options).format(date); // Вяртае гадзіну і хвіліну
 }
-
-
-
-
-// Функцыя для шыфравання паведамлення
-function encryptMessage(message, secretKey) {
-    // Шыфруем паведамленне з дапамогай AES і пераўтворым у радок
-    return CryptoJS.AES.encrypt(message, secretKey).toString();
-}
-
-function decryptMessage(encryptedMessage, secretKey) {
-    const bytes = CryptoJS.AES.decrypt(encryptedMessage, secretKey);
-    return bytes.toString(CryptoJS.enc.Utf8);
-}
-
 
 
 
@@ -188,14 +186,13 @@ function loadHistory() {
 }
 
 
-
-
 // Абслугоўваем падзею скролу
 messagesContainer.addEventListener('scroll', () => {
     if (messagesContainer.scrollTop === 0) {
         loadHistory();
     }
 });
+
 
 // Абнаўленне спісу паведамленняў пры новых паведамленнях
 socket.on('message', (message) => {
@@ -262,7 +259,6 @@ function sendMessage() {
 }
 
 
-
 const chatInput = document.getElementById("chatInput");
 
 chatInput.addEventListener("keydown", (event) => {
@@ -271,6 +267,15 @@ chatInput.addEventListener("keydown", (event) => {
         sendMessage();
     }
 });
+
+
+
+
+
+
+
+
+
 
 // Абработчык для кнопкі тэмы
 document.getElementById('theme').addEventListener('click', function () {
@@ -288,19 +293,26 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 });
 
+
+
+
+
+
 // Функцыя для выхаду
 function logout() {
     localStorage.removeItem('token');
     window.location.href = 'index.html';
 }
 
-
-
-
 const logoutButton = document.getElementById('logout');
 if (logoutButton) {
     logoutButton.addEventListener('click', logout);
 }
+
+
+
+
+
 
 // Абработчык націску на кнопку "очысціць чат"
 document.getElementById('cleenchat').addEventListener('click', function () {
